4d953b4f642e620654b000df7c44fab5bdd1c6e1
izesw <77286767+izesw@users.noreply.github.com>
2022-09-25 15:11:39 -0400

    re

---
+++ newbanner.py
@@ -0,0 +1,22 @@
+import sched, time
+import json
+
+s = sched.scheduler(time.time, time.sleep)
+def do_something(sc):
+    dictionary = {
+        "name": "sathiyajith",
+        "rollno": 56,
+        "cgpa": 8.6,
+        "phonenumber": "9976770500"
+    }
+    
+    # Serializing json
+    json_object = json.dumps(dictionary, indent=4)
+    
+    # Writing to sample.json
+    with open("banner.json", "w") as outfile:
+        outfile.write(json_object)
+    sc.enter(60, 1, do_something, (sc,))
+
+s.enter(60, 1, do_something, (s,))
+s.run()
